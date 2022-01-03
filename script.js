4d3ee9cdb3c91739b6b489641b13c4fe8de024a7
develop-vin <97022076+develop-vin@users.noreply.github.com>
2022-01-03 10:01:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("login");
+
+	var y = document.getElementById("signup");
+
+	var z = document.getElementById("switch");	
+
+	function signup() {
+		x.style.left = "-400px"
+		y.style.left = "50px"
+		z.style.left = "210px"
+	}	
+
+function login() {
+		x.style.left = "50px"
+		y.style.left = "450px"
+		z.style.left = ""
+	}	
---
+++ site/index.js
@@ -0,0 +1,14 @@
+var myName = "Vincent,";
+var yourName = prompt("what is your name");
+
+alert("My name is " + myName+ " welcome to my website " + yourName + " please sign up to play a web game" + "!");
+	
+if (yourName.length === prompt.length) {
+
+	alert("please you must write your name");
+	prompt("what is your name")
+} else{
+	alert("welcome")
+}
+
+
